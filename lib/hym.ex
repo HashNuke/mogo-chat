99ebb4d3c7d1e53a2ce7c41ba025a660a325026e
Akash Manohar J <akash@akash.im>
2014-01-15 17:42:50 +0530

    Add Hym files

---
+++ lib/hym.ex
@@ -0,0 +1,11 @@
+defmodule Hym do
+  use Application.Behaviour
+
+  @doc """
+  The application callback used to start this
+  application and its Dynamos.
+  """
+  def start(_type, _args) do
+    Hym.Dynamo.start_link([max_restarts: 5, max_seconds: 5])
+  end
+end
---
+++ lib/hym/dynamo.ex
@@ -0,0 +1,35 @@
+defmodule Hym.Dynamo do
+  use Dynamo
+
+  config :dynamo,
+    # The environment this Dynamo runs on
+    env: Mix.env,
+
+    # The OTP application associated with this Dynamo
+    otp_app: :hym,
+
+    # The endpoint to dispatch requests to
+    endpoint: ApplicationRouter,
+
+    # The route from which static assets are served
+    # You can turn off static assets by setting it to false
+    static_route: "/static"
+
+  # config :dynamo,
+  #   session_store: Session.CookieStore,
+  #   session_options:
+  #     [ key: "_hym_session",
+  #       secret: "dPFxY9JxtCRtXUpWOEd0+YRIUwlnG4tcuYbUza5AIKXxhFhiCAyR4NYCDD1C5YLt"]
+
+  config :dynamo,
+    session_store: Session.ETSStore,
+    session_options: [
+      table: :hym_sessions,
+      key: "_hym_session"
+    ]
+
+  # Default functionality available in templates
+  templates do
+    use Dynamo.Helpers
+  end
+end
