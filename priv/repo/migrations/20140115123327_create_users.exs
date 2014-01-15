d837ec45d7bbad9519b6d66fff4145140812c236
Akash Manohar J <akash@akash.im>
2014-01-15 18:14:46 +0530

    Update migration with role field

---
+++ priv/repo/migrations/20140115123327_create_users.exs
@@ -0,0 +1,11 @@
+defmodule Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def up do
+    "CREATE TABLE IF NOT EXISTS users(id serial primary key, first_name text, last_name text, email text, role text, encrypted_password text)"
+  end
+
+  def down do
+    "DROP TABLE users"
+  end
+end
