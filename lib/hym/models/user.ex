5455ca2aa1642f12b3071d0704b0833235fc7ed1
Akash Manohar J <akash@akash.im>
2014-01-15 18:14:32 +0530

    Add user model

--- lib/hym/models/user.ex
+++ lib/hym/models/user.ex
@@ -2,9 +2,10 @@ defmodule User do
   use Ecto.Model
 
   queryable "users" do
-    field :first_name,         :string
-    field :last_name,          :string
-    field :email,              :string
+    field :email,      :string
     field :encrypted_password, :string
+    field :role,       :string
+    field :first_name, :string
+    field :last_name,  :string
   end
 end
