227c29a13b6874bb33229b1185e845466944dba5
Akash Manohar J <akash@akash.im>
2014-01-18 21:35:09 +0530

    Fixed session router

---
+++ web/routers/sessions_api_router.ex
@@ -0,0 +1,52 @@
+defmodule SessionsApiRouter do
+  use Dynamo.Router
+  use Ecto.Query
+  import Cobalt.RouterUtils
+
+
+  get "/" do
+    user_id = get_session conn, :user_id
+    if user_id do
+      user = Repo.get(User, user_id)
+      attributes = Repo.attributes(user, ["email"])
+      json_response [user: attributes], conn
+    else
+      json_response [error: "no session"], conn
+    end
+  end
+
+
+  post "/" do
+    params = conn.params
+    login(conn, params["email"], params["password"])
+  end
+
+
+  delete "/" do
+    json_response [ok: "logged out"], delete_session(conn, :user_id)
+  end
+
+
+  defp login(conn, email, password) when email == nil or password == nil do
+    json_response [error: "Please check your login credentials."], conn, 401
+  end
+
+  defp login(conn, email, password) do
+    query = from u in User, where: u.email == ^email
+    users = Repo.all query
+
+    case length(users) > 0 do
+      true ->
+        user = users |> hd
+        if User.valid_password?(user, password) do
+          conn = put_session(conn, :user_id, user.id)
+          json_response [user: User.public_attributes(user)], conn
+        else
+          json_response [error: "Please check your login credentials."], conn, 401
+        end
+      false ->
+        json_response [error: "Maybe you don't have an account?"], conn, 401
+    end
+  end
+
+end
