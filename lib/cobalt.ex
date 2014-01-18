1f925e6e381054eb985b559b2e64e30d72ff0f6c
Akash Manohar J <akash@akash.im>
2014-01-18 16:09:23 +0530

    Create ets table for sessions

--- lib/cobalt.ex
+++ lib/cobalt.ex
@@ -6,6 +6,7 @@ defmodule Cobalt do
   application and its Dynamos.
   """
   def start(_type, _args) do
+    :ets.new(:cobalt_sessions, [:named_table, :public, {:read_concurrency, true}])
     Cobalt.Dynamo.start_link([max_restarts: 5, max_seconds: 5])
   end
 end
