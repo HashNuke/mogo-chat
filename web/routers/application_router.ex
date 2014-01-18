0b73103bef0aef68ca48df42a578850b01ca754c
Akash Manohar J <akash@akash.im>
2014-01-18 14:34:28 +0530

    Change title colors to #06c for uniformity

--- web/routers/application_router.ex
+++ web/routers/application_router.ex
@@ -5,12 +5,20 @@ defmodule ApplicationRouter do
     # Pick which parts of the request you want to fetch
     # You can comment the line below if you don't need
     # any of them or move them to a forwarded router
-    conn.fetch([:cookies, :params])
+    conn.fetch([:cookies, :session, :headers, :params, :body]).assign(:layout, "application")
   end
 
   # It is common to break your Dynamo into many
   # routers, forwarding the requests between them:
   # forward "/posts", to: PostsRouter
+
+
+  # forward "/api/sessions", to: SessionsApiRouter
+  # forward "/api/channels", to: ChannelsApiRouter
+  # forward "/api/users",    to: UsersApiRouter
+  # forward "/api/threads",  to: ThreadsApiRouter
+  # forward "/api/mails",    to: MailsApiRouter
+
 
   get "/" do
     conn = conn.assign(:title, "Welcome to Dynamo!")
