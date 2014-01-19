2d7aa2aee7329c4f68892e976316532097bee4c4
Akash Manohar J <akash@akash.im>
2014-01-19 20:00:21 +0530

    Add sample integration test using Hound

---
+++ test/integration/sessions_integration_test.exs
@@ -0,0 +1,22 @@
+defmodule SessionIntegrationTest do
+  use Cobalt.TestCase
+  use Hound.Helpers
+  import TestUtils
+
+  hound_session
+  truncate_db_after_test
+
+  test "returns OK" do
+    # user = User.new(email: "test@example.com", password: "password", first_name: "Test")
+    # |> User.encrypt_password()
+    # |> Repo.create
+
+    navigate_to app("/")
+    IO.inspect page_source()
+
+
+    # conn = post("/api/sessions", [{"email", "test@example.com"}, {"password", "password"}])
+    # IO.inspect conn.sent_body()
+    assert true == true
+  end
+end
