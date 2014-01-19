ca5873158610f6d5e5d77624d4f9ea1c96cda558
Akash Manohar J <akash@akash.im>
2014-01-19 19:56:28 +0530

    Move TestUtils to test_helper.exs

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,7 @@
 Dynamo.under_test(Cobalt.Dynamo)
 Dynamo.Loader.enable
 ExUnit.start
+Hound.start [driver: "phantomjs"]
 
 defmodule Cobalt.TestCase do
   use ExUnit.CaseTemplate
@@ -9,5 +10,19 @@ defmodule Cobalt.TestCase do
   setup do
     Dynamo.Loader.enable
     :ok
+  end
+end
+
+
+defmodule TestUtils do
+  def app(path) do
+    "http://localhost:#{Cobalt.Dynamo.config[:server][:port]}/#{path}"
+  end
+
+
+  def truncate_db_after_test do
+    table_names = ["users", "messages", "rooms"]
+    sql = "TRUNCATE TABLE #{Enum.join(table_names, ", ")} RESTART IDENTITY CASCADE;"
+    Repo.adapter.query(Repo, sql)
   end
 end
