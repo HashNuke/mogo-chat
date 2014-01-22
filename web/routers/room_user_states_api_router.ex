ffbdef58d41596fa345f0b6ad09f942ca8d53ba4
Akash Manohar J <akash@akash.im>
2014-01-22 15:15:32 +0530

    Add RoomUserStatesApiRouter

--- web/routers/room_user_states_api_router.ex
+++ web/routers/room_user_states_api_router.ex
@@ -3,57 +3,53 @@ defmodule RoomUserStatesApiRouter do
   use Ecto.Query
   import Cheko.RouterUtils
 
+  prepare do
+    authenticate_user!(conn)
+  end
 
   get "/" do
-    rooms = Repo.all Room
-    rooms_attributes = lc room inlist rooms do
-      Room.public_attributes(room)
+    user_id = get_session(conn, :user_id)
+    rooms = Repo.all(Room)
+
+    room_user_states = Enum.map rooms, fn(room)->
+      query = from r in RoomUserState, where: r.user_id == ^user_id and r.room_id == ^room.id
+      result = Repo.all query
+      if length(result) == 0 do
+        room_user_state = RoomUserState.new(user_id: user_id, room_id: room.id, joined: false)
+        Repo.create(room_user_state)
+      else
+        [room_user_state|_] = result
+        room_user_state
+      end
     end
-    json_response([rooms: rooms_attributes], conn)
+
+    room_user_states_attributes = lc room_user_state inlist room_user_states do
+      RoomUserState.public_attributes(room_user_state)
+    end
+
+    json_response([room_user_states: room_user_states_attributes], conn)
   end
 
 
-  post "/" do
+  put "/:room_user_state_id" do
+    room_user_state_id = conn.params[:room_user_state_id]
+    user_id = get_session(conn, :user_id)
+
     {:ok, params} = conn.req_body
     |> JSEX.decode
 
-    room_params = whitelist_params(params["room"], ["name"])
-    room = Room.new room_params
+    room_user_state_params = whitelist_params(params["room_user_state"], ["joined"])
+    query = from r in RoomUserState, where: r.id == ^room_user_state_id and r.user_id == ^user_id
+    [room_user_state] = Repo.get(RoomUserState, room_user_state_id)
+    room_user_state = room_user_state.update(room_user_state_params)
 
-    case Room.validate(room) do
+    case RoomUserState.validate(room_user_state) do
       [] ->
-        saved_room = Repo.create(room)
-        json_response [room: Room.public_attributes(saved_room)], conn
-      errors ->
-        json_response [errors: errors], conn
-    end
-  end
-
-
-  put "/:room_id" do
-    room_id = conn.params[:room_id]
-    {:ok, params} = conn.req_body
-    |> JSEX.decode
-
-    room_params = whitelist_params(params["room"], ["name"])
-    room = Repo.get(Room, room_id).update(room_params)
-    |> Room.encrypt_password()
-
-    case Room.validate(room) do
-      [] ->
-        :ok = Repo.update(room)
-        json_response [user: Room.public_attributes(room)], conn
+        :ok = Repo.update(room_user_state)
+        json_response [user: RoomUserState.public_attributes(room_user_state)], conn
       errors ->
         json_response [errors: errors], conn, 422
     end
-  end
-
-
-  delete "/:room_id" do
-    room_id = conn.params["room_id"]
-    room = Room.new(id: room_id)
-    Repo.delete room
-    json_response("", conn)
   end
 
 end
