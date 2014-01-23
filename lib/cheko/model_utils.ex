02758795a112215d2e902643d726b86b2b349c45
Akash Manohar J <akash@akash.im>
2014-01-23 14:20:10 +0530

    Add validation and necessary functions to Message model

--- lib/cheko/model_utils.ex
+++ lib/cheko/model_utils.ex
@@ -13,6 +13,15 @@ defmodule Cheko.ModelUtils do
           apply(accumulated_record, :"#{field}", [value])
         end
       end
+
+      def timestamp(ecto_datetime) do
+        datetime = {
+          {ecto_datetime.year, ecto_datetime.month, ecto_datetime.day},
+          {ecto_datetime.hour, ecto_datetime.min, ecto_datetime.sec}
+        }
+        "#{:qdate.to_string("Y-m-d", datetime)}T#{:qdate.to_string("H:i:s", datetime)}Z"
+      end
+
     end
   end
 
--- lib/cheko/models/message.ex
+++ lib/cheko/models/message.ex
@@ -10,4 +10,27 @@ defmodule Message do
     belongs_to :user, User
   end
 
+
+  def assign_message_type(record) do
+    if Regex.match?(%r/\n/, record.body) do
+      record.type("paste")
+    else
+      record.type("text")
+    end
+  end
+
+
+  validate message,
+    user_id: present(),
+    room_id: present(),
+    body:    present(),
+    type:    member_of(["text", "paste"]),
+    created_at: present()
+
+
+  def public_attributes(record) do
+    attrs = attributes(record, ["id", "room_id", "user_id", "body", "type"])
+    attrs ++ [created_at: timestamp(record.created_at)]
+  end
+
 end
