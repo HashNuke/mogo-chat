bd36c95848eb86be4d42538e505398885f835ee2
Akash Manohar J <akash@akash.im>
2014-01-23 19:53:55 +0530

    Rename test module appropriately

--- test/features/home_test.exs
+++ test/features/home_test.exs
@@ -1,11 +1,26 @@
-# Feature tests go through the Dynamo.under_test
-# and are meant to test the full stack.
-defmodule HomeTest do
+defmodule SessionTest do
   use Cheko.TestCase
   use Dynamo.HTTP.Case
+  import TestUtils
 
-  # test "returns OK" do
-  #   conn = get("/")
-  #   assert conn.status == 200
+  #TODO
+  # can't write these tests because Dynamo's Test connection doesn't parse
+  # request body, so params aren't set for connection
+
+  # truncate_db_after_test
+  #
+  # test_dynamo(Cheko.Dynamo)
+  # @endpoint Cheko.Dynamo
+  #
+  # test "should return user details after successful login" do
+  #   user = User.new(first_name: "Test", email: "test@example.com", password: "password")
+  #   user = User.encrypt_password(user)
+  #   Repo.create(user)
+  #
+  #   conn = post("/api/sessions", [{"email", "test@example.com"}, {"password", "password"}])
+  #   IO.inspect conn.sent_body
+  #
+  #   assert true
   # end
+
 end
