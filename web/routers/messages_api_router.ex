bdd87bc1aba2a9e07601ce92715c1ac7894d50d9
Akash Manohar J <akash@akash.im>
2014-02-01 16:35:52 +0530

    Update last_pinged_at when fetching messages

--- web/routers/messages_api_router.ex
+++ web/routers/messages_api_router.ex
@@ -9,6 +9,7 @@ defmodule MessagesApiRouter do
 
 
   get "/:room_id" do
+    user_id = get_session(conn, :user_id)
     before_message_id = conn.params[:before]
     after_message_id  = conn.params[:after]
     room = Repo.get Room, binary_to_integer(conn.params[:room_id])
@@ -38,6 +39,14 @@ defmodule MessagesApiRouter do
           where: m.room_id == ^room.id
     end
 
+
+    room_state_query = from r in RoomUserState,
+      where: r.room_id == ^room.id and r.user_id == ^user_id
+
+    [room_state] = Repo.all room_state_query
+    room_state.update([last_pinged_at: current_timestamp()])
+    |> Repo.update()
+
     messages_attributes = lc message inlist Repo.all(query) do
       Dict.merge Message.public_attributes(message), [user: User.public_attributes(message.user.get)]
     end
@@ -53,19 +62,12 @@ defmodule MessagesApiRouter do
 
     #TODO check if room with the room_id exists
     message_params = whitelist_params(params["message"], ["room_id", "body"])
-    {{year, month, day}, {hour, minute, seconds}} = :erlang.localtime()
-    created_at = Ecto.DateTime.new(
-      year: year,
-      month: month,
-      day: day,
-      hour: hour,
-      min: minute,
-      sec: seconds)
+
     message = Message.new(
       body: message_params["body"],
       room_id: binary_to_integer(message_params["room_id"]),
       user_id: user_id,
-      created_at: created_at
+      created_at: current_timestamp()
     ) |> Message.assign_message_type()
 
     case Message.validate(message) do
