70556de26317b72e4a089dec935cc529b6025d88
Akash Manohar J <akash@akash.im>
2014-02-13 11:16:10 +0530

    Check if heroku and use DATABASE_URL env var in read_db_config macro

--- lib/mogo_chat/app_config.ex
+++ lib/mogo_chat/app_config.ex
@@ -2,21 +2,26 @@ defmodule AppConfig do
 
   defmacro read_db_config do
     cond do
-      database_url = System.get_env("DB_URL") ->
-        database_url
+      System.get_env("STACK") == "cedar" ->
+        quote do
+          def url do
+            System.get_env("DATABASE_URL")
+          end
+        end
+
       File.exists?("config/database.json")->
         {:ok, config_json} = File.read "config/database.json"
         {:ok, config} = JSEX.decode config_json
         database_url = config["#{Mix.env}"]
+
+        quote do
+          def url do
+            unquote(database_url)
+          end
+        end
       true ->
         raise("Database details not found. Either create a config/database.json or set DATABASE_URL env")
     end
 
-    quote do
-      def url do
-        unquote(database_url)
-      end
-    end
   end
-
 end
