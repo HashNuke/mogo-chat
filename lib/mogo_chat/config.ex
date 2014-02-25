9dba5a6986b6461071301a5922c4fb01c3369d9f
Akash Manohar J <akash@akash.im>
2014-02-25 08:52:16 +0530

    Add config per env; Update Phoenix

---
+++ lib/mogo_chat/config.ex
@@ -0,0 +1,25 @@
+# Production
+defmodule MogoChat.Config do
+  use Phoenix.Config.App
+
+  config :router, port: 4000
+  config :plugs, code_reload: false
+end
+
+# Development
+defmodule MogoChat.Config.Dev do
+  use MogoChat.Config
+
+  config :router, port: 4000
+  config :plugs, code_reload: true
+  config :logger, level: :debug
+end
+
+# Test
+defmodule MogoChat.Config.Test do
+  use MogoChat.Config
+
+  config :router, port: 8888
+  config :plugs, code_reload: true
+  config :logger, level: :debug
+end
