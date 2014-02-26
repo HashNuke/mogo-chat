2abb0edd6cac882d6472a8f20efbf186865f9641
Akash Manohar J <akash@akash.im>
2014-02-26 09:20:45 +0530

    Simpler error handlers since pull requests to Phoenix have been merged into master

--- lib/mogo_chat/auth_error_handler.ex
+++ lib/mogo_chat/auth_error_handler.ex
@@ -9,18 +9,10 @@ defmodule MogoChat.AuthErrorHandler do
   def wrap(conn, _opts, fun) do
     try do
       fun.(conn)
-      IO.inspect "it executes fine"
     catch
-      kind, {{exception, application_trace}, _} ->
-        # Check if this error can be handled by this plug
-        if elem(exception, 0) == MogoChat.Errors.Unauthorized do
-          stacktrace = System.stacktrace
-          # send response to browser in case the error can be handled by this plug
-          html conn, 401, "Unauthorized! Please find some other property to trespass"
-        else
-          # if not raise the same exception again to be handled elsewhere
-          raise exception
-        end
+      kind, MogoChat.Errors.Unauthorized[message: reason] ->
+        stacktrace = System.stacktrace
+        html conn, 401, "Unauthorized! Please find some other property to trespass"
     end
   end
 end
