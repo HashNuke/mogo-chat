8d50a071f262e96401d9f9fbb0ea6b18a313f151
Akash Manohar J <akash@akash.im>
2014-05-06 18:05:49 +0530

    Update json parser and util

--- lib/mogo_chat/json_parser.ex
+++ lib/mogo_chat/json_parser.ex
@@ -2,29 +2,29 @@
 defmodule MogoChat.JsonParser do
   alias Plug.Conn
 
-  def parse(Conn[] = conn, "application", "json", _headers, opts) do
-    if conn.method != "GET" do
-      read_body(conn, Keyword.fetch!(opts, :limit))
-    else
-      { :next, conn }
-    end
+  def parse(%Conn{} = conn, "application", "json", _headers, opts) do
+    read_body(conn, Keyword.fetch!(opts, :limit))
   end
 
   def parse(conn, _type, _subtype, _headers, _opts) do
     { :next, conn }
   end
 
-  defp read_body(Conn[adapter: { adapter, state }] = conn, limit) do
-    case MogoChat.Util.read_body({ :ok, "", state }, "", limit, adapter) do
-      { :too_large, state } ->
-        { :too_large, conn.adapter({ adapter, state }) }
-      { :ok, body, state } ->
+
+  defp read_body(conn, limit) do
+    case HexWeb.Plug.read_body(conn, limit) do
+      { :too_large, conn } ->
+        { :too_large, conn }
+      { :ok, "", conn } ->
+        { :ok, [], conn }
+      { :ok, body, conn } ->
         case JSEX.decode(body) do
           { :ok, params } ->
-            { :ok, params, conn.adapter({ adapter, state }) }
+            { :ok, params, conn }
           _ ->
             raise MogoChat.Util.BadRequest, message: "malformed JSON"
         end
     end
   end
+
 end
