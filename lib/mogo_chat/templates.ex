f13ff43ff27e72512e15da7f8d6f48bf055d2c85
Akash Manohar J <akash@akash.im>
2014-08-08 22:12:37 +0530

    Use HexWeb's HTMLEngine

--- lib/mogo_chat/templates.ex
+++ lib/mogo_chat/templates.ex
@@ -1,6 +1,6 @@
 defmodule MogoChat.Templates do
   require EEx
 
-  EEx.function_from_file :def, :index, "templates/index.html.eex", [], [engine: HTMLEngine]
-  EEx.function_from_file :def, :message, "templates/message.html.eex", [:message], [engine: HTMLEngine]
+  EEx.function_from_file :def, :index, "templates/index.html.eex", [], [engine: HexWeb.Web.HTML.Engine]
+  EEx.function_from_file :def, :message, "templates/message.html.eex", [:message], [engine: HexWeb.Web.HTML.Engine]
 end
