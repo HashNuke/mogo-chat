388165985fa3c3267272099619c1352086d3f113
Akash Manohar J <akash@akash.im>
2014-08-08 22:59:48 +0530

    Configure Phoenix

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,19 @@
 use Mix.Config
 
+config :bcrypt, [mechanism: :port, pool_size: 4]
+
+config :phoenix, MogoChat.Router,
+  port: System.get_env("PORT"),
+  ssl: false,
+  code_reload: false,
+  static_assets: true,
+  cookies: true,
+  session_key: "_mogo_chat_key",
+  session_secret: (:crypto.strong_rand_bytes(64) |> :base64.encode_to_string |> to_string),
+  consider_all_requests_local: true
+
+config :phoenix, :logger, level: :error
+
 case Mix.env do
   :test ->
     config :firebrick, [db_url: "ecto://postgres_username:password@localhost/mogo_chat_test"]
@@ -8,5 +22,3 @@ case Mix.env do
   _ ->
     config :firebrick, [db_url: "ecto://postgres_username:password@localhost/mogo_chat_development"]
 end
-
-config :bcrypt, [mechanism: :port, pool_size: 4]
