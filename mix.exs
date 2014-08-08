8876845d45fb6d2e915db6bbbc456f4f24e52512
Akash Manohar J <akash@akash.im>
2014-08-08 20:11:28 +0530

    Update jsex

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule MogoChat.Mixfile do
       {:phoenix,  github: "phoenixframework/phoenix"},
       {:ecto,     "~> 0.2.3" },
       {:postgrex, "~> 0.5.0" },
-      {:jsex,     github: "talentdeficit/jsex", branch: "develop"},
+      {:jsex,     github: "talentdeficit/jsex"},
       {:qdate,    github: "choptastic/qdate" },
       {:bcrypt,   github: "irccloud/erlang-bcrypt"},
       {:uuid,     github: "okeuday/uuid"}
