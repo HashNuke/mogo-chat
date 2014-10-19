0451a88643e5846a9e116856193faa8f8c9cc89b
Akash Manohar J <akash@akash.im>
2014-10-19 22:03:53 +0530

    Update stuff in mix.exs

--- mix.exs
+++ mix.exs
@@ -3,16 +3,16 @@ defmodule MogoChat.Mixfile do
 
   def project do
     [ app: :mogo_chat,
-      version: "0.0.1",
+      version: "0.2",
       build_per_environment: true,
-      elixir: "~> 0.15.0",
+      elixir: "~> 1.0.1",
       deps: deps ]
   end
 
   # Configuration for the OTP application
   def application do
     [
-      applications: [:jazz, :phoenix, :bcrypt, :qdate, :uuid],
+      applications: [:phoenix, :bcrypt, :qdate, :uuid],
       mod: { MogoChat, []}
     ]
   end
@@ -21,10 +21,9 @@ defmodule MogoChat.Mixfile do
   defp deps do
     [
       {:cowboy,   "~> 1.0.0"},
-      {:phoenix,  github: "phoenixframework/phoenix"},
-      {:ecto,     "~> 0.2.3" },
-      {:postgrex, "~> 0.5.0" },
-      {:jazz, "0.2.0"},
+      {:phoenix,  "~> 0.5.0"},
+      {:ecto,     "~> 0.2.5"},
+      {:postgrex, "~> 0.6.0"},
       {:qdate,    github: "choptastic/qdate" },
       {:bcrypt,   github: "irccloud/erlang-bcrypt"},
       {:uuid,     github: "okeuday/uuid"}
